9357e31e055b38c5bf1a21b1096f030643a6b1af
jonathan sabogal <jsabogalto@outlook.com>
2025-02-21 15:25:35 -0500

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Images Galley</h1>
-      (initial frontend app)
+    <div>
+      <Header title = "Images Gallery"/>
     </div>
   );
 }
